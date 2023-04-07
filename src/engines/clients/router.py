19ecdf7873cb0d64c3dae5e523fd46c2b95fa112
Josh Crowley <jcrowley317@gmail.com>
2023-04-07 16:38:36 -0700

    create client tests

--- src/engines/clients/router.py
+++ src/engines/clients/router.py
@@ -3,23 +3,18 @@ from fastapi import APIRouter
 router = APIRouter()
 
 # not in endpoint specs
-# @router.get("/clients")
-# async def get_clients():
-# 	return {"message": "Got all clients"}
+@router.get("/clients")
+async def get_clients():
+	return {"message": "Got all clients"}
 
-@router.get("/clients/{client_id}")
-async def get_client(client_id: int):
-	return {"message": f"Got clients for {client_id}"}
+@router.get("/clients/{client_uuid}")
+async def get_client(client_uuid: str):
+	return {"message": f"Got clients for {client_uuid}"}
 
 @router.post("/clients")
 async def create_client():
 	return {"message": "Created client"}
 
-@router.put("/clients/{client_id}")
-async def update_client(client_id: int):
-	return {"message": f"Updated client for {client_id}"}
-
-# not in endpoint specs
-# @router.delete("/clients/{client_id}")
-# async def delete_client(client_id: int):
-# 	return {"message": f"Deleted client for {client_id}"}
+@router.put("/clients/{client_uuid}")
+async def update_client(client_uuid: str):
+	return {"message": f"Updated client for {client_uuid}"}
