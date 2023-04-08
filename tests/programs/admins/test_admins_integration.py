f0ca2cff7aeb8aa308e5d524acc722652fe96760
Josh Crowley <jcrowley317@gmail.com>
2023-04-07 20:52:00 -0700

    create program admins tests

---
+++ tests/programs/admins/test_admins_integration.py
@@ -0,0 +1,58 @@
+import pytest
+import httpx
+from fastapi import FastAPI
+from fastapi.testclient import TestClient
+from src.engines.programs.admins.router import router as admins_router
+
+app = FastAPI()
+app.include_router(admins_router)
+client = TestClient(app)
+
+
+@pytest.mark.asyncio
+async def test_integration_create_admin():
+	client_uuid = "test_client_uuid"
+	programs_uuid = "test_programs_uuid"
+	async with httpx.AsyncClient(app=app, base_url="http://test") as ac:
+		response = await ac.post(f"clients/{client_uuid}/programs/{programs_uuid}/admins")
+		assert response.status_code == 200
+		assert response.json() == {"message": "Created admin"}
+
+@pytest.mark.asyncio
+async def test_integration_get_admins():
+	client_uuid = "test_client_uuid"
+	programs_uuid = "test_programs_uuid"
+	async with httpx.AsyncClient(app=app, base_url="http://test") as ac:
+		response = await ac.get(f"clients/{client_uuid}/programs/{programs_uuid}/admins")
+		assert response.status_code == 200
+		assert response.json() == {"message": "Got all admins"}
+
+@pytest.mark.asyncio
+async def test_integration_get_admin():
+	client_uuid = "test_client_uuid"
+	programs_uuid = "test_programs_uuid"
+	admins_uuid = "test_admins_uuid"
+	async with httpx.AsyncClient(app=app, base_url="http://test") as ac:
+		response = await ac.get(f"clients/{client_uuid}/programs/{programs_uuid}/admins/{admins_uuid}")
+		assert response.status_code == 200
+		assert response.json() == {"message": f"Got admins for {admins_uuid}"}
+
+@pytest.mark.asyncio
+async def test_integration_update_admin():
+	client_uuid = "test_client_uuid"
+	programs_uuid = "test_programs_uuid"
+	admins_uuid = "test_admins_uuid"
+	async with httpx.AsyncClient(app=app, base_url="http://test") as ac:
+		response = await ac.put(f"clients/{client_uuid}/programs/{programs_uuid}/admins/{admins_uuid}")
+		assert response.status_code == 200
+		assert response.json() == {"message": f"Updated admin for {admins_uuid}"}
+
+@pytest.mark.asyncio
+async def test_integration_delete_admin():
+	client_uuid = "test_client_uuid"
+	programs_uuid = "test_programs_uuid"
+	admins_uuid = "test_admins_uuid"
+	async with httpx.AsyncClient(app=app, base_url="http://test") as ac:
+		response = await ac.delete(f"clients/{client_uuid}/programs/{programs_uuid}/admins/{admins_uuid}")
+		assert response.status_code == 200
+		assert response.json() == {"message": f"Deleted admin for {admins_uuid}"}
---
+++ tests/programs/admins/test_admins_unit.py
@@ -0,0 +1,46 @@
+from fastapi import FastAPI
+from fastapi.testclient import TestClient
+from src.engines.programs.admins.router import router as admins_router
+
+app = FastAPI()
+app.include_router(admins_router)
+client = TestClient(app)
+
+
+def test_get_admins():
+	client_uuid = "test_client_uuid"
+	program_uuid = "test_program_uuid"
+	response = client.get(f"/clients/{client_uuid}/programs/{program_uuid}/admins")
+	assert response.status_code == 200
+	assert response.json() == {"message": "Got all admins"}
+
+def test_get_admin():
+	client_uuid = "test_client_uuid"
+	program_uuid = "test_program_uuid"
+	admin_uuid = "test_admin_uuid"
+	response = client.get(f"/clients/{client_uuid}/programs/{program_uuid}/admins/{admin_uuid}")
+	assert response.status_code == 200
+	assert response.json() == {"message": f"Got admins for {admin_uuid}"}
+
+def test_create_admin():
+	client_uuid = "test_client_uuid"
+	program_uuid = "test_program_uuid"
+	response = client.post(f"/clients/{client_uuid}/programs/{program_uuid}/admins")
+	assert response.status_code == 200
+	assert response.json() == {"message": "Created admin"}
+
+def test_update_admin():
+	client_uuid = "test_client_uuid"
+	program_uuid = 'test_program_uuid'
+	admin_uuid = 'test_admin_uuid'
+	response = client.put(f"/clients/{client_uuid}/programs/{program_uuid}/admins/{admin_uuid}")
+	assert response.status_code == 200
+	assert response.json() == {"message": f"Updated admin for {admin_uuid}"}
+
+def test_delete_admin():
+	client_uuid = "test_client_uuid"
+	program_uuid = 'test_program_uuid'
+	admin_uuid = 'test_admin_uuid'
+	response = client.delete(f"/clients/{client_uuid}/programs/{program_uuid}/admins/{admin_uuid}")
+	assert response.status_code == 200
+	assert response.json() == {"message": f"Deleted admin for {admin_uuid}"}
