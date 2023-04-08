4fdbbe77c734fc71f097023915dbad6194d15e88
Josh Crowley <jcrowley317@gmail.com>
2023-04-07 20:43:13 -0700

    create client program tests

--- src/engines/programs/router.py
+++ src/engines/programs/router.py
@@ -7,7 +7,7 @@ async def get_programs():
 	return {"message": "Got all programs"}
 
 @router.get("/clients/{client_uuid}/programs/{program_uuid}")
-async def get_program(program_uuid: int):
+async def get_program(program_uuid: str):
 	return {"message": f"Got programs for {program_uuid}"}
 
 @router.post("/clients/{client_uuid}/programs")
@@ -15,10 +15,10 @@ async def create_program():
 	return {"message": "Created program"}
 
 @router.put("/clients/{client_uuid}/programs/{program_uuid}")
-async def update_program(program_uuid: int):
+async def update_program(program_uuid: str):
 	return {"message": f"Updated program for {program_uuid}"}
 
 # should only work if there are no segments or events associated with the program
 @router.delete("/clients/{client_uuid}/programs/{program_uuid}")
-async def delete_program(program_uuid: int):
+async def delete_program(program_uuid: str):
 	return {"message": f"Deleted program for {program_uuid}"}
