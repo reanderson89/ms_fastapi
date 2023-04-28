6d7e57af89ffe2d8b2640dfa2aff1fa3907a63e6
Ryan Green <ryan@rgreen.dev>
2023-04-28 12:47:44 -0700

    changing email field to str

--- app/models/users/users_service_models.py
+++ app/models/users/users_service_models.py
@@ -1,6 +1,5 @@
 from sqlmodel import Field, SQLModel
-from pydantic import EmailStr
-from typing import Optional, List
+from typing import Optional
 
 class UsersServiceModel(SQLModel, table=True):
 	__tablename__ = "user_service"
@@ -8,8 +7,7 @@ class UsersServiceModel(SQLModel, table=True):
 	uuid: str = Field(primary_key=True, index=True)
 	user_uuid: str = Field(default=None, description="UUID set from `users` table")
 	service_uuid: str = Field(default=None)
-	#service_user_id: List[EmailStr] = Field(default_factory=lambda:[], index=True, max_length=255, description="user email address")
-	service_user_id: EmailStr = Field(default=None, index=True)
+	service_user_id: str = Field(default=None, index=True, max_length=255)
 	service_user_screenname: str = Field(default=None, max_length=255)
 	service_user_name: str = Field(default=None, max_length=255)
 	service_access_token: str = Field(default=None, max_length=255)
@@ -19,7 +17,6 @@ class UsersServiceModel(SQLModel, table=True):
 	time_updated: int = None
 
 class UsersServiceUpdate(SQLModel, table=False):
-	service_user_id: Optional[str] = Field(default=None, max_length=255)
 	service_user_screenname: Optional[str] = Field(default=None, max_length=255)
 	service_user_name: Optional[str] = Field(default=None, max_length=255)
 	service_access_token: Optional[str] = Field(default=None, max_length=255)
