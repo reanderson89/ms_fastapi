f84b753f9e2c14c581c2b830ed540b4b2a8ec5c0
Robert Anderson <67164106+reanderson89@users.noreply.github.com>
2023-05-25 10:54:17 -0700

    fixes to client_budget_model.py (#73)
    * fixed client_budget_model so that classes are extending off a base class model

--- app/models/clients/client_budget_models.py
+++ app/models/clients/client_budget_models.py
@@ -3,7 +3,21 @@ from app.models.base_class import Base, BasePydantic
 from typing import Optional
 
 
-class ClientBudgetBase():
+class ClientBudgetBase(BasePydantic):
+	uuid: Optional[str] 
+	client_uuid: Optional[str] 
+	budget_9char: Optional[str] 
+	parent_9char: Optional[str] 
+	name: Optional[str] 
+	value: Optional[int] 
+	time_created: Optional[int] 
+	time_updated: Optional[int] 
+	active: Optional[bool]
+	budget_type: Optional[int] 
+
+class ClientBudgetModel(Base):
+	__tablename__ = "client_budget"
+	
 	uuid: Mapped[str] = mapped_column(default=None, primary_key=True, index=True)
 	client_uuid: Mapped[str] = mapped_column(default=None, index=True)
 	budget_9char: Mapped[str] = mapped_column(default=None, index=True)
@@ -14,9 +28,6 @@ class ClientBudgetBase():
 	time_updated: Mapped[int] = mapped_column(default=None)
 	active: Mapped[bool] = mapped_column(default=True)
 	budget_type: Mapped[int] = mapped_column(default=0)
-
-class ClientBudgetModel(ClientBudgetBase, Base):
-	__tablename__ = "client_budget"
 
 class ClientBudgetCreate(BasePydantic):
 	budget_9char: Optional[str] = None
@@ -34,10 +45,10 @@ class ClientBudgetUpdate(BasePydantic):
 	active: Optional[bool]
 	budget_type: Optional[int]
 
-class ClientBudgetExpanded(BasePydantic):
+class ClientBudgetExpanded(ClientBudgetBase):
 	client: Optional[dict]
 	subbudgets_expanded: Optional[list[dict]]
 
-class ClientBudgetShortExpand(BasePydantic):
+class ClientBudgetShortExpand(ClientBudgetBase):
 	client: Optional[str]
 	subbudgets: Optional[dict]
