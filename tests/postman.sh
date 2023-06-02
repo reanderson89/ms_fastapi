283b6ce7daf238b3735a7a922c532e870c95a5e6
Clark Ritchie <clark.ritchie@blueboard.com>
2023-06-02 08:36:34 -0700

    simple bash script to run Postman tests with Newman

---
+++ tests/postman.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+#
+# These are all the "known good" Postman tests, run against a running Docker container:
+#
+# docker exec -it milestones_api /app/tests/postman.sh
+#
+
+set -e
+
+SOURCE_DIR=/app/tests/postman/
+tests=(
+   "$SOURCE_DIR"/Milestones_admins.postman_collection.json
+   "$SOURCE_DIR"/Milestones_budgets.postman_collection.json
+   "$SOURCE_DIR"/Milestones_clients.postman_collection.json
+   "$SOURCE_DIR"/Milestones_events.postman_collection.json
+   "$SOURCE_DIR"/Milestones_message_templates.postman_collection.json
+   "$SOURCE_DIR"/Milestones_messages.postman_collection.json
+   "$SOURCE_DIR"/Milestones_programs.postman_collection.json
+   "$SOURCE_DIR"/Milestones_users.postman_collection.json
+)
+
+for test in "${tests[@]}"; do
+    [ -f "$test" ] || break
+   newman run $test
+done
