77422895ee17c9d303abf3cf77bb3b4668e00b5b
Clark Ritchie <clark.ritchie@blueboard.com>
2023-06-06 16:26:41 -0700

    guessing here...

--- app/configs/database_configs.py
+++ app/configs/database_configs.py
@@ -21,24 +21,35 @@ class LocalDB(BaseDB):
 	PASSWD: str = os.environ.get('MYSQL_PASSWORD', 'password')
 	DB: str = os.environ.get('MYSQL_DATABASE', 'blueboard_milestones')
 
+class DevDB(BaseDB):
+	# these env vars are coming from docker-compose.yml
+	HOST: str =  os.environ.get('MYSQL_HOSTNAME', 'localhost')
+	PORT: int = os.environ.get('MYSQL_PORT', 32776)
+	USER: str = os.environ.get('MYSQL_USER', 'root')
+	PASSWD: str = os.environ.get('MYSQL_PASSWORD', 'password')
+	DB: str = os.environ.get('MYSQL_DATABASE', 'blueboard_milestones')
+
 @dataclass
 class StagingDB(BaseDB):
-	HOST: str = '10.100.1.22'
-	PORT: int = 3306
-	USER: str = 'USERNAME'
-	PASSWD: str = 'PASSWORD'
-	DB: str = 'milestone_staging'
+	# these env vars are coming from docker-compose.yml
+	HOST: str =  os.environ.get('MYSQL_HOSTNAME', 'localhost')
+	PORT: int = os.environ.get('MYSQL_PORT', 32776)
+	USER: str = os.environ.get('MYSQL_USER', 'root')
+	PASSWD: str = os.environ.get('MYSQL_PASSWORD', 'password')
+	DB: str = os.environ.get('MYSQL_DATABASE', 'blueboard_milestones')
 
 @dataclass
 class ProdDB(BaseDB):
-	HOST: str = '10.100.1.21'
-	PORT: int = 3306
-	USER: str = 'USERNAME'
-	PASSWD: str = 'PASSWORD'
-	DB: str = 'milestone_prod'
+	# these env vars are coming from docker-compose.yml
+	HOST: str =  os.environ.get('MYSQL_HOSTNAME', 'localhost')
+	PORT: int = os.environ.get('MYSQL_PORT', 32776)
+	USER: str = os.environ.get('MYSQL_USER', 'root')
+	PASSWD: str = os.environ.get('MYSQL_PASSWORD', 'password')
+	DB: str = os.environ.get('MYSQL_DATABASE', 'blueboard_milestones')
 
 configs = {
 	'local':LocalDB(),
+	'dev':DevDB(),
 	'staging':StagingDB(),
 	'prod':ProdDB()
 }
