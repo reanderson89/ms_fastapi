c4b21a3b6f0bdc39169a7a96d3df0e4672fbfdc3
Owen Plambeck <33457704+oplambeck@users.noreply.github.com>
2023-06-09 09:39:47 -0700

    Disabling auth requirements for local dev (#94)
    Co-authored-by: Owen Plambeck <owenplambeck@Owens-Laptop.local>

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -1,13 +1,21 @@
+import os
+
 from fastapi import APIRouter, Depends
 from app.utilities.auth import get_token, UnAuthedMessage
 from starlette import status
 from .v1 import v1router
 from .v1.users.auth.auth_routers import router as auth_router
 
-routers = APIRouter(
-        dependencies=[Depends(get_token)],
-        responses={status.HTTP_401_UNAUTHORIZED: dict(model=UnAuthedMessage)}
-    )
+ENV = os.environ['ENV']
+
+if ENV == 'local':
+    routers = APIRouter()
+else:
+    routers = APIRouter(
+            dependencies=[Depends(get_token)],
+            responses={status.HTTP_401_UNAUTHORIZED: dict(model=UnAuthedMessage)}
+        )
+
 
 auth_routers = APIRouter()
 
