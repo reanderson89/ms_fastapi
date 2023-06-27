bc89b71116fc91c2e89f2de4e8786b899e15809b
Owen Plambeck <33457704+oplambeck@users.noreply.github.com>
2023-06-27 15:11:27 -0700

    verify email clickable link (#125)

--- app/libraries/sparkpost.py
+++ app/libraries/sparkpost.py
@@ -8,14 +8,14 @@ _scriptname = "ThirdParty.SparkPost"
 SPARKPOST_KEY = os.environ["SPARKPOST_KEY"]
 sp = SparkPost(SPARKPOST_KEY)
 
-
+BASE_URL = "https://milestones-dev.blueboard.app/auth/verify-email-token?token="
 
 async def send_auth_email(user_service: UserServiceModelDB):
 
     response = sp.transmissions.send(
         use_sandbox=False,
         recipients=[user_service.service_user_id],
-        html="<p>" + user_service.login_token + "</p>",
+        html="<a href=" + BASE_URL + user_service.login_token + ">VERIFY EMAIL</a>",
         from_email="no-reply@mail.blueboard.app",
         subject="Blueboard Login Token"
     )
