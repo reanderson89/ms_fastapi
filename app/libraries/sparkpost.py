82481af6f85759e6a64b50eeea904057e26901e0
Clark Ritchie <clarkritchie@users.noreply.github.com>
2023-07-18 14:19:33 -0700

    make the base url for token redemption into a variable (#151)
    Co-authored-by: Josh Crowley <jcrowley317@gmail.com>

--- app/libraries/sparkpost.py
+++ app/libraries/sparkpost.py
@@ -9,14 +9,15 @@ _scriptname = "ThirdParty.SparkPost"
 SPARKPOST_KEY = os.environ["SPARKPOST_KEY"]
 sp = SparkPost(SPARKPOST_KEY)
 
-BASE_URL = "https://milestones-dev.blueboard.app/auth/verify-email-token?token="
+BASE_URL = os.getenv("BASE_URL", "https://milestones.blueboard.app")
+REDEEM_URL = f"{BASE_URL}/auth/verify-email-token?token="
 
 async def send_auth_email(user_service: UserServiceModelDB):
 
     response = sp.transmissions.send(
         use_sandbox=False,
         recipients=[user_service.service_user_id],
-        html="<a href=" + BASE_URL + user_service.login_token + ">VERIFY EMAIL</a>",
+        html="<a href=" + REDEEM_URL + user_service.login_token + ">VERIFY EMAIL</a>",
         from_email="no-reply@mail.blueboard.app",
         subject="Blueboard Login Token"
     )
