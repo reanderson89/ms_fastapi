05f6ba83319560da1548cc74269f5d12f84c1ecc
Owen Plambeck <33457704+oplambeck@users.noreply.github.com>
2023-08-03 14:07:40 -0700

    removing nsclae devs from seed users  (#194)
    * removing nsclae devs
    
    * Tabs to spaces

--- app/seed_data/seed_clients/seed_client_users.py
+++ app/seed_data/seed_clients/seed_client_users.py
@@ -456,37 +456,8 @@ josh = {
         "time_birthday": int(faker.date_time_between(start_date="-50y", end_date="-18y").timestamp()) #we dont ask for birthday during client user creation
     }
 
-dan = {
-        "client_uuid": "ca723b34b08e4e319c8d2e6770815679c69aaf4a8e574f518b1e34",
-        "work_email": "daniel@nscale.io",
-        "first_name": "Daniel",
-        "last_name": "O'Neill",
-        "title": "Frontend",
-        "manager_uuid": "06ad1e1f05a61ab1ac423d5a6fb969193305145100c888a069eaacbf",
-        "department": "Frontend",
-        "admin": 2,
-        "time_start": int(faker.date_time_between(start_date="-1y", end_date="now").timestamp()),
-        "time_hire": int(faker.date_time_between(start_date="-1y", end_date="now").timestamp()),
-        "active": 1,
-        "employee_id": faker.pystr(),
-        "time_birthday": int(faker.date_time_between(start_date="-50y", end_date="-18y").timestamp())}
 
-mike = {
-        "client_uuid": "ca723b34b08e4e319c8d2e6770815679c69aaf4a8e574f518b1e34",
-        "work_email": "michael@nscale.io",
-        "first_name": "Michael",
-        "last_name": "Lindenau",
-        "title": "Frontend",
-        "manager_uuid": "06ad1e1f05a61ab1ac423d5a6fb969193305145100c888a069eaacbf",
-        "department": "Frontend",
-        "admin": 2,
-        "time_start": int(faker.date_time_between(start_date="-1y", end_date="now").timestamp()),
-        "time_hire": int(faker.date_time_between(start_date="-1y", end_date="now").timestamp()),
-        "active": 1,
-        "employee_id": faker.pystr(),
-        "time_birthday": int(faker.date_time_between(start_date="-50y", end_date="-18y").timestamp())}
-
-new = [owen, jason, josh, ryan, clark, mike, dan]
+new = [owen, jason, josh, ryan, clark]
 
 
 async def generate_client_users(clients: list):
