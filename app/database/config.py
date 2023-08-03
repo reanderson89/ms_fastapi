8f55fe10ba4ff267c28fc5855ab77ef311e62c7c
Josh Crowley <jcrowley317@gmail.com>
2023-08-03 11:26:12 -0700

    test fix for db disconnection (#193)
    * add pool_pre_ping to engine
    
    * Tabs to spaces

--- app/database/config.py
+++ app/database/config.py
@@ -7,7 +7,7 @@ from app.configs import db_config
 DATABASE_URL = f"mysql+pymysql://{db_config.USER}:{db_config.PASSWD}@{db_config.HOST}:{db_config.PORT}/{db_config.DB}"
 
 echo_sql_output: bool = os.environ.get("ENV", "local").lower() == "local"
-engine = create_engine(DATABASE_URL, echo=echo_sql_output)
+engine = create_engine(DATABASE_URL, pool_pre_ping=True, echo=echo_sql_output)
 
 # TODO: refactor logging to use python logging, allowing for more control
 #   uncomment the following lines and remove 'echo=True' from engine_create()
