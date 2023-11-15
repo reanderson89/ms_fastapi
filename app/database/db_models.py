2e89327a280757f32165fe563e7e0116e07d0871
Robert Anderson <67164106+reanderson89@users.noreply.github.com>
2023-11-14 17:15:33 -0800

    added db_models (#241)
    * added db_models
    
    * added db_models with comments

--- app/database/db_models.py
+++ app/database/db_models.py
@@ -3,6 +3,20 @@
 # from app.configs.database_configs import db_config
 
 
+from burp.models.client import ClientModelDB # noqa: F401
+from burp.models.client_user import ClientUserModelDB # noqa: F401
+from burp.models.client_award import ClientAwardModelDB # noqa: F401
+from burp.models.client_budget import ClientBudgetModelDB # noqa: F401
+from burp.models.message import MessageModelDB # noqa: F401
+from burp.models.program_admin import AdminModelDB # noqa: F401
+from burp.models.program_award import ProgramAwardModelDB # noqa: F401
+from burp.models.program_event import ProgramEventModelDB # noqa: F401
+from burp.models.program_rule import ProgramRuleModelDB # noqa: F401
+from burp.models.program import ProgramModelDB # noqa: F401
+from burp.models.segment_award import SegmentAwardModelDB # noqa: F401
+from burp.models.segment_design import SegmentDesignModelDB # noqa: F401
+from burp.models.segment_rule import SegmentRuleModelDB # noqa: F401
+from burp.models.segment import SegmentModelDB # noqa: F401
 from burp.models.base_models import Base # noqa: F401
 
 # # this is the Alembic Config object, which provides
