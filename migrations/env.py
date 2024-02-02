7156c17bd7d2bab638de73bdda0eb4e9c63a8897
Owen Plambeck <33457704+oplambeck@users.noreply.github.com>
2024-02-02 13:41:26 -0800

    patch to add alembic lock (#370)

--- migrations/env.py
+++ migrations/env.py
@@ -1,6 +1,6 @@
 from logging.config import fileConfig
 
-from sqlalchemy import engine_from_config
+from sqlalchemy import engine_from_config, text
 from sqlalchemy import pool
 
 from alembic import context
@@ -68,9 +68,12 @@ def run_migrations_online() -> None:
         context.configure(
             connection=connection, target_metadata=target_metadata
         )
-
-        with context.begin_transaction():
-            context.run_migrations()
+        try:
+            with context.begin_transaction():
+                connection.execute(text("SELECT pg_advisory_xact_lock(10000);"))
+                context.run_migrations()
+        except:
+            raise Exception
 
 
 if context.is_offline_mode():
