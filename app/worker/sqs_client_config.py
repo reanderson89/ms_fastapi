614b6d025c0fa14633fd6bcaa8ff17e4dbdd8440
Clark Ritchie <clark.ritchie@blueboard.com>
2024-02-13 09:10:18 -0800

    change QUEUE_REGION to AWS_REGION and default to us-west-1

--- app/worker/sqs_client_config.py
+++ app/worker/sqs_client_config.py
@@ -6,7 +6,7 @@ from mypy_boto3_sqs import SQSClient
 
 from app.worker.utils import WorkerUtils
 
-QUEUE_REGION = os.environ.get("QUEUE_REGION", "us-east-1")
+AWS_REGION = os.environ.get("AWS_REGION", "us-west-1")
 
 
 class SQSClientSingleton:
@@ -19,12 +19,12 @@ class SQSClientSingleton:
             with cls._lock:
                 if cls._instance is None:
                     endpoint_url = WorkerUtils.get_base_uri(queue_url)
-                    cls._instance: SQSClient = boto3.client("sqs", region_name=QUEUE_REGION, endpoint_url=endpoint_url)
+                    cls._instance: SQSClient = boto3.client("sqs", region_name=AWS_REGION, endpoint_url=endpoint_url)
         return cls._instance
 
     @classmethod
     def reconnect(cls, queue_url):
         with cls._lock:
             endpoint_url = WorkerUtils.get_base_uri(queue_url)
-            cls._instance: SQSClient = boto3.client("sqs", region_name=QUEUE_REGION, endpoint_url=endpoint_url)
+            cls._instance: SQSClient = boto3.client("sqs", region_name=AWS_REGION, endpoint_url=endpoint_url)
         return cls._instance
